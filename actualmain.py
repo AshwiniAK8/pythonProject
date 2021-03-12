f24d82f96b5c66911eb7cd5ab00e940c9a9a6369
AshwiniAK <mdm18b006@iiitdm.ac.in>
2021-03-12 12:53:11 +0530

    x)

--- actualmain.py
+++ actualmain.py
@@ -1,5 +1,5 @@
 import serial
-
+import webbrowser
 from kivymd.app import MDApp
 from kivy.lang import Builder
 from kivy.core.window import Window
@@ -22,6 +22,19 @@ class Home(Screen):
 class Schedule(Screen):
     pass
 class Track(Screen):
+    def val1(self):
+        ser = serial.Serial('COM4', 9600)
+        # b = 'b Latitude in Decimal Degrees : 13.025846\r\n'
+        # a= 'b Longitude in Decimal Degrees : 43.245\r\n'
+        b = str(ser.readline())
+        a = str(ser.readline())
+        print(b)
+        print(a)
+        b1 = b.split()
+        a1 = a.split()
+        lat1 = b1[-1]
+        long1 = a1[-1]
+        webbrowser.open('https://www.google.com/maps/search/?api=1&query='+lat1[:-5]+','+long1[:-5])
 
     pass
 sm = ScreenManager()
@@ -31,17 +44,7 @@ sm.add_widget(Schedule(name='schedule'))
 sm.add_widget(Track(name='track'))
 class Autitech(MDApp):
     d = 120
-    ser = serial.Serial('COM4', 9600)
-    # b = 'b Latitude in Decimal Degrees : 13.025846\r\n'
-    # a= 'b Longitude in Decimal Degrees : 43.245\r\n'
-    b = str(ser.readline())
-    a = str(ser.readline())
-    print(b)
-    print(a)
-    b1 = b.split()
-    a1 = a.split()
-    lat1 = b1[-1]
-    long1 = a1[-1]
+
     class ContentNavigationDrawer(BoxLayout):
         pass
 
